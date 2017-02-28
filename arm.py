8bcb4e20e2d0739de5cf0d5e0035a0328565f8d7
Emanuel Raad <raademang@gmail.com>
2017-02-27 21:09:06 -0500

    1

--- arm.py
+++ arm.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
+
+def initGPIO():
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(40, GPIO.OUT)
 
@@ -26,15 +29,13 @@ class Motor():
 
 def main():
     print "Hello World!"
+    armServo = Motor(40)
+    armServo.start(Motor.CENTER)
 
     try:
         while True:
-            p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-            time.sleep(1) # sleep 1 second
-            p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-            time.sleep(1) # sleep 1 second
-            p.ChangeDutyCycle(12.5) # turn towards 180 degree
-            time.sleep(1) # sleep 1 second
+            armServo.turnToAndSleep(Motor.OPEN, 2)
+            armServo.turnToAndSleep(Motor.CLOSE, 2)
     except KeyboardInterrupt:
         p.stop()
         GPIO.cleanup()
