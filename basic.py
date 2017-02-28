e42b5a280ceac983968457db5b2c4bc4ef448e11
Emanuel Raad <raademang@gmail.com>
2017-02-27 22:22:12 -0500

    1

--- basic.py
+++ basic.py
@@ -10,13 +10,13 @@ p = GPIO.PWM(40, 50)
 p.start(7.5)
 
 try:
-        while True:
+    while True:
         p.ChangeDutyCycle(7.5)  # turn towards 90 degree
         time.sleep(1) # sleep 1 second
         p.ChangeDutyCycle(2.5)  # turn towards 0 degree
         time.sleep(1) # sleep 1 second
         p.ChangeDutyCycle(12.5) # turn towards 180 degree
-                time.sleep(1) # sleep 1 second
+        time.sleep(1) # sleep 1 second
 except KeyboardInterrupt:
     p.stop()
-        GPIO.cleanup()
+    GPIO.cleanup()
