7908f12440db44dbe1a0ce9783453451931b85d5
Emanuel Raad <raademang@gmail.com>
2017-04-09 19:49:24 -0400

    slow

--- threadedClaw.py
+++ threadedClaw.py
@@ -36,10 +36,15 @@ class Motor(Thread):
             elif self.mode == 2:
                 position = self.speedIncrements[self.counter]
                 self.p.ChangeDutyCycle(position)
-                print("Position: {}").format(position)
-                time.sleep(0.2)
-                if self.counter < len(self.speedIncrements):
+                print("Position: {}\tCounter:{}").format(position, self.counter)
+                time.sleep(0.3)
+                if self.counter < len(self.speedIncrements) - 1:
                     self.counter += 1
+                else:
+                    self.setPosition(position)
+                    self.counter = 0
+                    self.speedIncrements = []
+                    self.mode = 1
 
     def setRunning(self, isRunning):
         self.running = isRunning
@@ -48,41 +53,58 @@ class Motor(Thread):
         self.mode = 1
         self.position = position
 
-    def setPositionSlow(self, position):
+    def setPositionSlow(self, position, h):
         self.mode = 2
-        increment = (self.position - position)/0.2
-        n = int(math.ceil(increment))
+        if (position < self.position):
+            step = -1 * h
+        else:
+            step = h
+
+        increment = (self.position - position) / step
+        n = int(math.ceil(abs(increment)))
         for i in range(0, n):
-            if i == n:
+            if i == n - 1:
                 self.speedIncrements.append(position)
             else:
-                self.speedIncrements.append(self.position + n*i)
+                self.speedIncrements.append(self.position + step * i)
 
     def turnToAndSleep(self, duty, sleep):
         if (self.debug):
             print "{}: Going to {}. Sleeping for {}".format(self.name, duty, sleep)
-        self.p.ChangeDutyCycle(duty)
+        # self.p.ChangeDutyCycle(duty)
         time.sleep(sleep)
 
     def stop(self):
         self.running = False
         self.position = 0
-        self.p.stop()
+        # self.p.stop()
 
     def setDebug(self, debug):
         self.debug = debug
 
-
 def main():
-    PIN_ARM = 12
-    PIN_CLAW = 40
-
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(PIN_ARM, GPIO.OUT)
-    GPIO.setup(PIN_CLAW, GPIO.OUT)
 
+    PIN_ARM = 12
+    GPIO.setup(PIN_ARM, GPIO.OUT)
     armMotor = Motor(PIN_ARM, 'arm', Motor.OPEN)
+
+    '''
+    PIN_CLAW = 40
+    GPIO.setup(PIN_CLAW, GPIO.OUT)
     claw = Motor(PIN_CLAW, 'arm', 9.5)
+    '''
+
+    armMotor.start()
+
+    armMotor.setPositionSlow(2.5, 1)
+    time.sleep(5)
+    armMotor.setPositionSlow(12.5, 1)
+
+    armMotor.stop()
+    GPIO.cleanup()
+
+    '''
     armMotor.start()
     claw.start()
     time.sleep(5)
@@ -97,6 +119,7 @@ def main():
     armMotor.stop()
     claw.stop()
     GPIO.cleanup()
+    '''
 
 if __name__ == "__main__":
     main()
