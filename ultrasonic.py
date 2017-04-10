ef6addcb2a906afed14948aeb46355657eb0c167
Emanuel Raad <raademang@gmail.com>
2017-04-09 20:52:38 -0400

    disntace

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO                    #Import GPIO library
 import time                                #Import time library
+from scipy import median
 
 class Ultrasonic:
   def __init__(self, trig, echo):
@@ -10,30 +11,38 @@ class Ultrasonic:
     GPIO.setup(self.echo, GPIO.IN)
 
   def getDistance(self, sampleSize = 10, waitTime = 0.00001):
-    # set Trigger to HIGH
-    GPIO.output(self.trig, True)
+    distances = []
+    time.sleep(2)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(self.trig, False)
+    for i in range(0, sampleSize):
+      # set Trigger to HIGH
+      GPIO.output(self.trig, True)
 
-    StartTime = time.time()
-    StopTime = time.time()
+      # set Trigger after 0.01ms to LOW
+      time.sleep(waitTime)
+      GPIO.output(self.trig, False)
 
-    # save StartTime
-    while GPIO.input(self.echo) == 0:
       StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(self.echo) == 1:
       StopTime = time.time()
 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-    return distance
+      # save StartTime
+      while GPIO.input(self.echo) == 0:
+        StartTime = time.time()
+
+      # save time of arrival
+      while GPIO.input(self.echo) == 1:
+        StopTime = time.time()
+
+      # time difference between start and arrival
+      TimeElapsed = StopTime - StartTime
+      # multiply with the sonic speed (34300 cm/s)
+      # and divide by 2, because there and back
+      distance = (TimeElapsed * 34300) / 2
+
+      if ((distance > 2) and (distance < 400)):
+        distances.append(distance)
+
+    return median(distances)
 
 
 def main():
